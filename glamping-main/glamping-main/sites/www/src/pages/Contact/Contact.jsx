4955b78171e1bd01ab7f8f69257ba5d97334333a
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-11-06 00:39:43 +0100

    Add Hero to Contact + MyList - create StaysList jsx

--- glamping-main/glamping-main/sites/www/src/pages/Contact/Contact.jsx
+++ glamping-main/glamping-main/sites/www/src/pages/Contact/Contact.jsx
@@ -1,1 +1,15 @@
-export default function Contact() {}
+import Hero from "../../components/Hero/Hero";
+import heroContact from "../../assets/image_03.jpg";
+
+export default function Contact() {
+  return (
+    <>
+      <Hero
+        variant="simple"
+        backgroundImage={heroContact}
+        title="Kontakt Gitte"
+        minHeight={{ xs: "80vh", md: "100vh" }}
+      />
+    </>
+  );
+}
--- glamping-main/glamping-main/sites/www/src/pages/MyList/MyList.jsx
+++ glamping-main/glamping-main/sites/www/src/pages/MyList/MyList.jsx
@@ -1,1 +1,15 @@
-export default function MyList() {}
+import Hero from "../../components/Hero/Hero";
+import heroMyList from "../../assets/image_05.jpg";
+
+export default function MyList() {
+  return (
+    <>
+      <Hero
+        variant="simple"
+        backgroundImage={heroMyList}
+        title="Min liste"
+        minHeight={{ xs: "80vh", md: "100vh" }}
+      />
+    </>
+  );
+}
