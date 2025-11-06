05676657aad05dcef55f98142411452bfffa1c9e
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-11-06 14:53:34 +0100

    updated backend to include datapoint called header for stay.model and stay.route

--- glamping-main/glamping-main/lib/db/models/stay.model.mjs
+++ glamping-main/glamping-main/lib/db/models/stay.model.mjs
@@ -1,16 +1,15 @@
-import mongoose, { Schema } from 'mongoose';
-mongoose.set('runValidators', true);
+import mongoose, { Schema } from "mongoose";
+mongoose.set("runValidators", true);
 
 const stayScheme = new Schema({
-    
-    title: { type: String},
-    description: { type: String},
-    numberOfPersons: { type: String },
-    price: { type: Number },
-    includes: { type: Array },
-    image: { type: String, default: '/stays/no-stays.jpg' },
-    created: { type: Date, default : new Date() },
-
+  title: { type: String },
+  header: { type: String },
+  description: { type: String },
+  numberOfPersons: { type: String },
+  price: { type: Number },
+  includes: { type: Array },
+  image: { type: String, default: "/stays/no-stays.jpg" },
+  created: { type: Date, default: new Date() },
 });
 
-export default mongoose.models.stay || mongoose.model('stay', stayScheme);
+export default mongoose.models.stay || mongoose.model("stay", stayScheme);
